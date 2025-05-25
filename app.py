9bab762d1d65519b7c563bad485803b0d150f496
shimseonjo <33996918+shimseonjo@users.noreply.github.com>
2025-05-25 14:32:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -20,21 +20,21 @@ class SearchResult:
         return f"{self.symbol}: {self.name}"
 
 
-st.title("서학 개미를 위한 AI 투자보고서 생성 서비스")
+st.title("AI 투자 보고서 생성 서비스")
 
-query = st.text_input("회사명", "Apple")
+query = st.text_input("회사명", "Apple Ins.")
 hits = stock_search(query)['hits']
 search_results = [SearchResult(hit) for hit in hits]
 
 selected = st.selectbox("검색 결과 리스트", search_results)
+stock = Stock(selected.symbol)
 
-tabs = ["회사 기본 정보", "AI 투자 보고서"]
+tabs = ["회사 정보", "AI 투자 보고서"]
 tab1, tab2 = st.tabs(tabs)
 
 with tab1:
-    stock = Stock(selected.symbol)
-    st.header(selected)
 
+    st.header(str(selected))
     st.write(stock.get_basic_info())
     st.write(stock.get_financial_statement())
 
